d654ca8efae43240ac72c299b672ccfc53915b0f
fanzee <fan.zee@qq.com>
2018-11-04 23:20:44 +0800

    update

---
+++ Python/列表简介/3.3/world.py
@@ -0,0 +1,9 @@
+tourist_places = ["beijing", "shanghai", "california", "provence", "melbourne"]
+print(tourist_places)
+print("\n")
+print(sorted(tourist_places))
+print(tourist_places)
+print("\n")
+print(sorted(tourist_places, reverse=True))
+print(tourist_places)
+print("\n")
